1676ea5a508ced149f07dd0ec18af5c278951e32
guicoelhodev <gs.coelho@outlook.com>
2023-04-05 16:38:21 -0300

    chore: try to add public directory to deploy on vercel

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,6 +23,9 @@ export default {
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
+  generate: {
+    dir: "public",
+  },
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
