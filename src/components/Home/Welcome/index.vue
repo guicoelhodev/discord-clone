554e393ff423c3666abb06c648f1d67903bc1cc8
guicoelhodev <gs.coelho@outlook.com>
2023-04-05 21:49:44 -0300

    feat: add animation to show intro, after that, the code shows current view

--- src/components/Home/Welcome/index.vue
+++ src/components/Home/Welcome/index.vue
@@ -1,15 +1,72 @@
-<script setup lang="ts">
-import { Icon } from "@iconify/vue2";
-</script>
-
 <template>
   <div
     id="welcome-div"
-    class="absolute w-screen h-screen bg-stone-950 flex items-center justify-center"
+    class="z-1 absolute inset-0 w-screen h-screen bg-stone-950 text-xl flex items-center justify-center"
   >
-    <section>
-      <p class="text-white">sdhjidsjkk</p>
-      <Icon icon="logos:discord-icon" />
+    <section class="intro-section" id="discord-intro">
+      <p>
+        This project is just a clone UI <br />
+        to pratice my front-end skills
+      </p>
+      <Icon icon="fa6-brands:discord" width="80" class="mt-4" />
+    </section>
+
+    <section class="intro-section" id="tools-intro">
+      <p>Was created with Vue and Tailwind</p>
+
+      <aside class="flex">
+        <Icon icon="mdi:vuejs" width="80" />
+        <Icon icon="mdi:tailwind" width="80" />
+      </aside>
+
+      <p>I hope that u enjoy it</p>
     </section>
   </div>
 </template>
+
+<script setup lang="ts">
+import { Icon } from "@iconify/vue2";
+import { gsap } from "gsap";
+import { onMounted } from "vue";
+
+const fadeInAnimation = (divId: string, initialDelay: number) => {
+  gsap.to(divId, {
+    opacity: 1,
+    duration: 2,
+    delay: initialDelay,
+  });
+  gsap.to(divId, {
+    opacity: 0,
+    duration: 1.5,
+    delay: initialDelay + 2.5,
+    onComplete: () => {
+      document.getElementById(divId)?.remove();
+    },
+  });
+};
+
+const removeAbsoluteWindow = () => {
+  gsap.to("#welcome-div", {
+    y: "-100%",
+    duration: 0.5,
+    delay: 12,
+    onComplete: () => {
+      gsap.to("#welcome-div", { display: "none" });
+
+      return window.sessionStorage.setItem("@HAS_INTRO_CLONE", "already seen");
+    },
+  });
+};
+
+onMounted(() => {
+  const getIntroStatus = window.sessionStorage.getItem("@HAS_INTRO_CLONE");
+
+  if (!getIntroStatus) {
+    fadeInAnimation("#discord-intro", 1);
+    fadeInAnimation("#tools-intro", 7);
+    removeAbsoluteWindow();
+  } else {
+    gsap.to("#welcome-div", { display: "none" });
+  }
+});
+</script>
--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -1,9 +1,31 @@
-<script lang="ts" setup>
-import Welcome from "./Welcome/index.vue";
-</script>
-
 <template>
   <div>
     <Welcome />
+
+    <div>
+      <section
+        id="home-view"
+        class="opacity-0 w-screen h-screen bg-white text-black grid place-content-center"
+      >
+        <h1 class="text-5xl">React > Vue</h1>
+        <h2 class="text-3xl">Styled components > Tailwind</h2>
+      </section>
+    </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { gsap } from "gsap";
+import { onMounted, ref } from "vue";
+import Welcome from "./Welcome/index.vue";
+
+onMounted(() => {
+  const getIntroStatus = window.sessionStorage.getItem("@HAS_INTRO_CLONE");
+
+  if (getIntroStatus) {
+    gsap.to("#home-view", { opacity: 1, delay: 1, duration: 0.8 });
+  } else {
+    gsap.to("#home-view", { opacity: 1, delay: 13, duration: 0.8 });
+  }
+});
+</script>
