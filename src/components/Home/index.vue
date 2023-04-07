bcb9317f084aa69f4e10a43edc038f60fae2a9ab
guicoelhodev <gs.coelho@outlook.com>
2023-04-07 13:35:28 -0300

    feat: add header component in HomePage

--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -1,11 +1,12 @@
 <template>
-  <div>
+  <div class="bg-blue-600">
+    <Header />
     <Welcome />
 
     <div>
       <section
         id="home-view"
-        class="opacity-0 w-screen h-screen bg-white text-black grid place-content-center"
+        class="opacity-0 w-screen h-screen text-black grid place-content-center"
       >
         <h1 class="text-5xl">React > Vue</h1>
         <h2 class="text-3xl">Styled components > Tailwind</h2>
@@ -17,7 +18,9 @@
 <script lang="ts" setup>
 import { gsap } from "gsap";
 import { onMounted, ref } from "vue";
+
 import Welcome from "./Welcome/index.vue";
+import Header from "../Global/Header/index.vue";
 
 onMounted(() => {
   const getIntroStatus = window.sessionStorage.getItem("@HAS_INTRO_CLONE");
