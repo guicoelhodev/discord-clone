63b700f00340c3605d8617092e4c644fe1dcd6c2
guicoelhodev <gs.coelho@outlook.com>
2023-04-09 00:22:04 -0300

    feat: create dropdownMenu link when device is samller then 768 px

--- src/components/Global/Header/index.vue
+++ src/components/Global/Header/index.vue
@@ -2,19 +2,18 @@
 import { Icon } from "@iconify/vue2";
 import { computed, onMounted, Ref, ref } from "vue";
 import NavLinks from "@/data/navLinks.json";
+import Dropdown from "@/components/Global/Header/Dropdown/index.vue";
 
-type INavLinks = { slug: String; path: string }[];
+export type INavLink = { slug: String; path: string };
 
-const navLinks: Ref<INavLinks> = ref(NavLinks.links);
+const navLinks: Ref<INavLink[]> = ref(NavLinks.links);
 
 const randomId = computed(() => Math.random().toString(36).substring(2, 9));
 </script>
 
 <template>
   <section>
-    <header
-      class="m-auto max-w-7xl p-4 text-white flex justify-between items-center gap-4"
-    >
+    <header class="m-auto max-w-7xl p-4 text-white flex items-center gap-4">
       <article class="flex gap-2 items-center">
         <Icon icon="fa6-brands:discord" width="40" color="#fff" />
         <p class="roboto font-extrabold tracking-wide text-lg">DISCORD</p>
@@ -25,20 +24,22 @@ const randomId = computed(() => Math.random().toString(36).substring(2, 9));
       >
         <nuxt-link
           v-for="link in navLinks"
-          :key="randomId + link.slug"
+          :key="randomId + link"
           :to="link.path"
           class="font-semibold hover:underline hover:decoration-1"
           >{{ link.slug }}</nuxt-link
         >
       </nav>
 
-      <aside>
+      <aside class="ml-auto">
         <button
           class="bg-white transition-colors duration-200 text-slate-950 py-2 px-4 text-sm rounded-full hover:shadow-lg hover:text-blue-700"
         >
           Login
         </button>
       </aside>
+
+      <Dropdown :links="navLinks" />
     </header>
   </section>
 </template>
