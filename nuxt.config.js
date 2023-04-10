0143efae84b880e5c42e299ad0c472251fed827e
guicoelhodev <gs.coelho@outlook.com>
2023-04-09 21:45:08 -0300

    build: remove generate public folder

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,9 +41,7 @@ export default {
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-  generate: {
-    dir: "public",
-  },
+ 
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
